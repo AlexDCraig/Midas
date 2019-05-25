74bb3741eecd4c1d9aaf24e8da49bb5c168d2f8d
AlexDHoffer <alexdchoffer@gmail.com>
2019-05-24 17:23:29 -0700

    Setup express as static file server

--- alex_node/server.js
+++ alex_node/server.js
@@ -1,23 +1,15 @@
-var http = require('http');
 var url = require('url');
-var fs = require('fs');
 var path = require('path');
 var express = require('express');
-var header = {'Content-Type': 'text/html'};
 var app = express();
+
 app.use(express.static(path.join(__dirname, 'templates')));
 
-http.createServer(function (request, response) {
+app.get('/', function(req, res) {
     var query = url.parse(request.url, true);
     var filename = query.pathname;
     var filepath = './templates' + filename;
-    fs.readFile(filepath, function (error, data) {
-        if (error) {
-            response.writeHead(404, header);
-            return response.end("404 Not Found");
-        }
-        response.writeHead(200, header);
-        response.write(data);
-        return response.end();
-    });
-}).listen(8080);
+    res.render(__dirname + filepath);
+});
+
+app.listen(8080);
