69c35b550e1f7882727a05f07f840af97c0f9eaf
AlexDHoffer <alexdchoffer@gmail.com>
2019-06-06 17:03:17 -0700

    Tidy up

--- deploy_locally_k8s.ps1
+++ deploy_locally_k8s.ps1
@@ -13,24 +13,26 @@ kubectl delete --all pods
 docker build -t midas-web:v0 midas/
 docker build -t midas-data:v0 midas-data/
 
-# Create two deployments, one based off the local node-server image and one based off of a remotely pulled Jenkins image.
-kubectl apply -f midas-web-deployment.yaml
-kubectl apply -f jenkins-deployment.yaml
+# Create three deployments: one off the local node-server image, one based off of a remotely pulled Jenkins image, one off the data processing local image.
+kubectl apply -f deployments/midas-web-deployment.yaml
+kubectl apply -f deployments/jenkins-deployment.yaml
+kubectl apply -f deployments/midas-data-deployment.yaml
 
 # Expose the node-server deployment and the Jenkins deployment.
 # We want NodePort type. LoadBalancer is a type native to cloud services.
 kubectl expose deployment midas-web-deployment --type=NodePort --port=80
-kubectl expose deployment jenkins --type=NodePort --port=81
+kubectl expose deployment jenkins --type=NodePort --port=8080
 
 # Get the externally accessible (to your own network) URLs.
 minikube service midas-web-deployment --url
 minikube service jenkins --url
 
-# Access the url in your browser by taking the exact url given from the command above and appending the filename to it (e.g. /home.html).
+# Access the midas url in your browser by taking the exact url/port combo given from the command above and appending the filename to it (e.g. /home.html).
+# Acces the Jenkins url in your browser by just taking the exact url/port combo.
 
 # Expose to the outside world through the VirtualBox VM using an ingress.
 # Use with caution.
 # https://github.com/nginxinc/kubernetes-ingress/issues/158 has a good template for what this should look like
-# kubectl apply -f midas-web-ingress.yml
+# kubectl apply -f ingress/midas-web-ingress.yml
 # ssh -i ~/.minikube/machines/minikube/id_rsa docker@$(minikube ip) -L \*:30000:0.0.0.0:30000
 # Load in browser: minikube ip, then copy paste that plus target port into browser
