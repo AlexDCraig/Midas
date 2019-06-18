9ff48bb023e6e8a09d879c3113eb0c905aff0a07
AlexDHoffer <alexdchoffer@gmail.com>
2019-06-17 21:29:24 -0700

    Put all the pieces together.
    Link K8s, database, NodeJS and templating service together to produce a
    working concept of Midas.

--- midas/server.js
+++ midas/server.js
@@ -11,34 +11,38 @@ var mongoAccessorString = "mongodb://" + argv["database_info"];
 var MongoClient = require('mongodb').MongoClient;
 var databaseName = "financial_data_db";
 var client = new MongoClient(mongoAccessorString);
+var db = null;
+var overallFinancesCollection = null;
+
 client.connect(function(err) {
     assert.equal(null, err);
     console.log('Connected to the mongoDB database server.');
-    var db = client.db(databaseName);
-    var overallFinancesCollection = db.collection("overall_finances_collection");
-    var overallFinancesData = overallFinancesCollection.find().toArray()
-        .then(function (financeData) {
-            return financeData;
-        });
-
-    console.log(overallFinancesData);
-
-  /*  cursor.each(function(err, doc) {
-        console.log(doc);
-    }); */
-
-    client.close();
+    db = client.db(databaseName);
+    overallFinancesCollection = db.collection("overall_finances_collection");
 });
+
+var overallDataPromise = () => (
+    new Promise((resolve, reject) => {
+        overallFinancesCollection.find().toArray()
+            .then(function(result){
+                resolve(result);
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+        })
+);
 
 // Handle statically served files on the fileserver.
 var app = express();
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'templates')));
 
 // Main entry point for web page.
-app.get('/', function(req, res) {
-    var query = url.parse(req.url, true);
-    var filename = query.pathname;
-    var filepath = './templates' + filename;
+app.get('/home', function(req, res) {
+    overallDataPromise().then(function(result) {
+        res.render('home.ejs', { overallFinancialSeries: result });
+    });
 });
 
 app.listen(80);
