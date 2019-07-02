50cecfc330ddad38bfa5fb45d19eac281d45988e
AlexDHoffer <alexdchoffer@gmail.com>
2019-07-01 17:23:12 -0700

    Make special directory for GKE deployment.

--- cloud/gcp/prepare_for_gke.py
+++ cloud/gcp/prepare_for_gke.py
@@ -2,12 +2,9 @@ import os
 
 project_id = os.environ['PROJECT_ID']
 base_string = 'gcr.io/{}'.format(project_id)
-old_midas_data = 'midas-data:v0'
-midas_data = '{}/{}'.format(base_string, old_midas_data)
 old_midas_web = 'midas-web:v0'
 midas_web = '{}/{}'.format(base_string, old_midas_web)
-base_filepath = os.path.expanduser("~/Midas/deployments")
-midas_data_filepath = '{}/midas-data-deployment.yaml'.format(base_filepath)
+base_filepath = os.path.expanduser("~/Midas/cloud/gcp/Midas/deployments")
 midas_web_filepath = '{}/midas-web-deployment.yaml'.format(base_filepath)
 
 def replace_strings_in_deployment(filepath, old_string, new_string):
@@ -20,7 +17,6 @@ def replace_strings_in_deployment(filepath, old_string, new_string):
         f.write(amended_text)
 
 def prep_for_gke():
-    replace_strings_in_deployment(midas_data_filepath, old_midas_data, midas_data)
     replace_strings_in_deployment(midas_web_filepath, old_midas_web, midas_web)
 
 prep_for_gke()
