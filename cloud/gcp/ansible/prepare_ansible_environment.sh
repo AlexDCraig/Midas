489742e1a63ca11cdb01bbfbdd00be917275a85b
AlexDHoffer <alexdchoffer@gmail.com>
2019-07-23 02:33:20 +0000

    Add Ansible for provisioning a GKE cluster.
    This commit adds the files necessary to provision a GKE cluster with Ansible.

--- cloud/gcp/ansible/prepare_ansible_environment.sh
+++ cloud/gcp/ansible/prepare_ansible_environment.sh
@@ -3,6 +3,12 @@
 xargs sudo yum -y install < yum-requirements.txt
 sudo pip install -r requirements.txt
 
-# Run a playbook.
+# Run the playbook.
 # ansible-playbook -i hosts midas.yaml
+
+# Run only the VM provisioner.
+# ansible-playbook -i hosts midas.yaml --tags="midas_data_vm"
+
+# Run only the Kubernetes cluster provisioner.
+# ansible-playbook -i hosts midas.yaml --tags="midas_gke_cluster"
 
