17ddb837f4c4e3a25194daee1cf0a0cc6e4ebccf
AlexDHoffer <alexdchoffer@gmail.com>
2019-07-23 19:20:29 -0700

    Tell GKE to look for images in my Docker Hub.

--- cloud/gcp/deploy_on_gke.sh
+++ cloud/gcp/deploy_on_gke.sh
@@ -5,26 +5,20 @@
 
 git clone https://github.com/AlexDHoffer/Midas.git
 cd Midas
-export PROJECT_ID="$(gcloud config get-value project -q)"
 
-# Build and tag the web interface..
-docker build -t gcr.io/${PROJECT_ID}/midas-web:v0 midas/
-docker tag midas-web:v0 gcr.io/${PROJECT_ID}/midas-web:v0
-
-# Push the image to the Google registry associated with your GKE project.
-gcloud auth configure-docker --quiet
-docker push gcr.io/${PROJECT_ID}/midas-web:v0
+# Pull down the web app from Alex's Docker Hub.
+docker pull alexdchoffer/midas-web:v0
 
 # Configure the database.
 kubectl apply -f deployments/midas-mongo-deployment.yaml
 kubectl expose deployment midas-mongo-deployment --type=LoadBalancer --port=27017
 
+kubectl apply -f deployments/midas-web-deployment.yaml
+kubectl expose deployment midas-web-deployment --type=LoadBalancer --port=80
+
 # Modify the midas-web deployment to directly refer to the image we just built, tagged, and pushed.
 # Then, tell the deployment to always search for an image to pull.
 python cloud/gcp/prepare_for_gke.py
-
-kubectl apply -f deployments/midas-web-deployment.yaml
-kubectl expose deployment midas-web-deployment --type=LoadBalancer --port=80
 
 # SSH into data VM: gcloud compute ssh crdhost --command ...
 # Run script in data VM (midas-data-deployment) that publishes data to midas-mongo service.
