3ee7f0dca6e16db1ac93270381a0cd62c54fe7eb
AlexDHoffer <alexdchoffer@gmail.com>
2019-07-24 20:56:10 -0700

    Add rudimentary Prometheus calls to local NodeJS.
    Mostly functions as a proof of concept of the NodeJS Prometheus
    exporter. More interesting data should be logged in the future.
    Additionally, cloud support would be nice, for example a Prometheus pod
    that logs the results of the web service.

--- midas/server.js
+++ midas/server.js
@@ -8,6 +8,9 @@ var argv = require('minimist')(process.argv.slice(2));
 var mongoAccessorString = "mongodb://" + argv["database_info"];
 
 // Handle database interactions.
+// For local testing: run a mongodb instance either in the background or another window.
+// For example, on Windows, this looks like: C:\"Program Files"\MongoDB\Server\4.0\bin\mongo.exe
+// You can then see the UI in your browser with 127.0.0.1/x.
 var MongoClient = require('mongodb').MongoClient;
 var databaseName = "financial_data_db";
 var client = new MongoClient(mongoAccessorString);
@@ -38,11 +41,39 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'templates')));
 
-// Main entry point for web page.
+// Set up Prometheus instrumentation for app performance.
+const Prometheus = require('prom-client');
+var responseTime = require('response-time')
+
+const httpRequestDurationMicroseconds = new Prometheus.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['route'],
+  // RT in interval [0.1, 500] ms
+  buckets: [0.10, 5, 15, 50, 100, 200, 300, 400, 500]
+});
+
+app.use(responseTime(function(req, res, time) {
+    try {
+        httpRequestDurationMicroseconds
+        .labels(req.route.path)
+        .observe(time);
+    } catch (error) {
+        console.log(error);
+    }
+}));
+
+// Entry point for main web page.
 app.get('/home', function(req, res) {
     overallDataPromise().then(function(result) {
         res.render('home.ejs', { overallFinancialSeries: result });
     });
 });
+
+// Endpoint to view Prometheus metrics.
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', Prometheus.register.contentType)
+  res.end(Prometheus.register.metrics())
+})
 
 app.listen(80);
